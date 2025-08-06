79f663504ea2c3e1f65dc8b5f5374c46a4256078
jdub4asdfg <junweilee72@gmail.com>
2025-08-06 18:08:34 +0800

    changing the hostname of the current process

--- main.go
+++ main.go
@@ -13,27 +13,32 @@ func main() {
 		if err := run(); err != nil {
 			log.Fatal(err)
 		}
+	case "sub":
+		if err := sub(); err != nil {
+			log.Fatal(err)
+		}
 	default:
 		log.Fatal("invalid command")
 	}
 }
 
 func run() error {
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"sub"}, os.Args[2:]...)...)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// Creating a new user namespace first because a new UTS namespace doesn't give me root privileges
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUSER | syscall.CLONE_NEWUTS,
+
 		UidMappings: []syscall.SysProcIDMap{
-			{ContainerID: 0, HostID: os.Getuid(), Size: 1}, // Makes the process think it has root privs but it's just running as a user on host
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
 		},
 		GidMappings: []syscall.SysProcIDMap{
-			{ContainerID: 0, HostID: os.Getgid(), Size: 1}, // Same logic but for groups
+			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
 		},
+
 		Credential: &syscall.Credential{
 			Uid: 0,
 			Gid: 0,
@@ -44,5 +49,23 @@ func run() error {
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func sub() error {
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	syscall.Sethostname([]byte("container"))
+
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
